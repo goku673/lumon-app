fbd3f03569da80203003b2e14f2c795cb82f3b9a
goku673 <zapatamorato@gmail.com>
2025-05-01 00:42:32 -0400

    Fix Select and Option Components

--- src/common/option.js
+++ src/common/option.js
@@ -1,7 +1,11 @@
 const Option = ({ value, label, className, ...props }) => (
-    <option className={className} value={value} {...props}>
-      {label}
-    </option>
-  );
+  <option className={className} value={value} {...props}>
+    {label}
+  </option>
+);
   
-  export default Option;
+export default Option;
+
+
+
+  
