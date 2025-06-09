f104dd3d9db58ae83c187f41bbf94b1b843a4404
Maurizm <89363295+Maurizm@users.noreply.github.com>
2025-06-08 22:26:26 -0400

    modalContainer

--- src/components/modal/modalContainer.js
+++ src/components/modal/modalContainer.js
@@ -1,3 +1,8 @@
+
+// Componente ModalContainer que define la estructura principal del modal.
+// Se usa `bg-white rounded-lg shadow-lg w-full max-w-md overflow-hidden` 
+// para proporcionar estilos de fondo, bordes redondeados, sombra y tamaño controlado.
+
 export const ModalContainer = ({ children, className = "" }) => {
     return <div className={`bg-white rounded-lg shadow-lg w-full max-w-md overflow-hidden ${className}`}>{children}</div>
   }
