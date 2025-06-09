d3f3796b22f466d79ec64e91f9033ba4bbdab580
Maurizm <89363295+Maurizm@users.noreply.github.com>
2025-06-08 21:54:36 -0400

    detail-inscription check

--- src/app/(pages)/detail-inscription/page.js
+++ src/app/(pages)/detail-inscription/page.js
@@ -1,3 +1,7 @@
+// Este componente está diseñado para mostrar detalles de una inscripción,
+// asegurando que solo los usuarios autenticados puedan acceder a la información.
+// Se utiliza "use client" para indicar que debe ejecutarse en el lado del cliente.
+
 "use client";
 import React from "react";
 import DetailInscriptionComponent from "@/components/detailIncription";
