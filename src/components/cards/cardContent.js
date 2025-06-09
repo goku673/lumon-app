cb0992a7d6c8dffac6eb211e5722fa4350b6f9c1
Maurizm <89363295+Maurizm@users.noreply.github.com>
2025-06-08 22:05:28 -0400

    cards content check

--- src/components/cards/cardContent.js
+++ src/components/cards/cardContent.js
@@ -1,9 +1,9 @@
-"use client";
+"use client";  // Indica que este componente se ejecutará en el cliente, útil en aplicaciones Next.js.
 
 const CardContent = ({ className = "", children, ...props }) => (
   <div className={`p-6 pt-0 ${className}`} {...props}>
-    {children}
-  </div>
+    {children} 
+  </div> // Renderiza los elementos hijos dentro del CardContent
 );
 
 export default CardContent;
