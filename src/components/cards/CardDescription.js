2c98e1620f947b97958512e7914536e3428edbf2
Maurizm <89363295+Maurizm@users.noreply.github.com>
2025-06-08 22:06:42 -0400

    CardDescription check

--- src/components/cards/CardDescription.js
+++ src/components/cards/CardDescription.js
@@ -3,7 +3,7 @@
 const CardDescription = ({ className = "", children, ...props }) => (
   <p className={`text-sm text-gray-500 ${className}`} {...props}>
     {children}
-  </p>
+  </p>// Renderiza el contenido dentro del CardDescription.
 );
 
 export default CardDescription;
