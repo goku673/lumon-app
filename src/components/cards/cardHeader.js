bfcf66cc401a5c1a3e4658eb137c8b0f788c6f21
Maurizm <89363295+Maurizm@users.noreply.github.com>
2025-06-08 22:14:28 -0400

    cardHeader check

--- src/components/cards/cardHeader.js
+++ src/components/cards/cardHeader.js
@@ -1,9 +1,11 @@
-"use client";
+"use client";// Indica que este componente se ejecutará en el cliente, útil en aplicaciones Next.js.
+
 
 const CardHeader = ({ className = "", children, ...props }) => (
   <div className={`flex flex-col space-y-1.5 p-6 ${className}`} {...props}>
     {children}
-  </div>
+  </div>// Renderiza los elementos hijos dentro del CardHeader.
+
 );
 
 export default CardHeader;
