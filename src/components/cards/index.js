3adf32173f2cfd68c723e0dc31fb51d73526ba9f
Maurizm <89363295+Maurizm@users.noreply.github.com>
2025-06-08 22:18:19 -0400

    index check

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,3 +1,6 @@
+// Exportación centralizada de componentes relacionados con tarjetas.
+// Esto permite importar fácilmente todos los componentes desde un solo archivo.
+
 export { default as Card } from "./card";
 export { default as CardHeader } from "./cardHeader";
 export { default as CardTitle } from "./cardTitle";
